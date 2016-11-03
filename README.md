7ff6938a5533b7a7ae0df79c4f733e009346062d
Denis <denis@dedeweb.fr>
2016-11-03 18:07:00 +0100

    some minor changes

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # tamerbooth
+/!\ this app is in early stage of devlopment, not working yet !
 A photobooth web app, used to control a DSLR connected to computer. 
+
+backend made using express server. Setup inspered by  https://javascriptrocks.wordpress.com/2016/06/04/express-with-angular-cli-in-5-minutes/
+
+# install
+
+	$ npm install
+	$ ng build
+	$ node dist/app
+
+website is launched on 127.0.0.1:3000
+
+
+### Credits
+
+* somewhat inspired by https://github.com/porkbuns/shmile
+
+
+
+
--- config/karma.conf.js
+++ config/karma.conf.js
@@ -4,7 +4,10 @@ module.exports = function (config) {
     frameworks: ['jasmine'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-chrome-launcher')
+      require('karma-chrome-launcher'),
+      require( 'karma-phantomjs-launcher' ),
+      //require( 'karma-remap-istanbul' ),
+      //require( 'angular-cli/plugins/karma' )
     ],
     customLaunchers: {
       // chrome setup for travis CI using chromium
@@ -36,7 +39,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'/*, 'Chrome'*/],
     singleRun: false
   });
 };
