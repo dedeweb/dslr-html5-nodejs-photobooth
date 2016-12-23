1581e4058b2ad92701f6b2f3d1853d8be241dc38
Denis <denis@dedeweb.fr>
2016-12-23 17:22:35 +0100

    adding cropper

--- admin/src/app/calibrate-webcam/calibrate-webcam.component.ts
+++ admin/src/app/calibrate-webcam/calibrate-webcam.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 import {CameraService} from '../camera.service';
+import { LogService } from 'log.service';
+declare var Cropper : any;
 
 @Component({
 	selector: 'app-calibrate-webcam',
@@ -8,7 +10,12 @@ import {CameraService} from '../camera.service';
 })
 export class CalibrateWebcamComponent implements OnInit {
 	public webcamImgSrc : string = "";
-	constructor(private cameraService : CameraService) {
+	private cropper: any;
+	
+	constructor(
+		private cameraService : CameraService,
+		private el: ElementRef,
+		private logger: LogService) {
 		var that = this;
 		cameraService.onWebcamImageReceived = function (image) {
 			that.webcamImgSrc = image;
@@ -17,6 +24,35 @@ export class CalibrateWebcamComponent implements OnInit {
 	
 	ngOnInit() {
 	}
+	
+	public initCropper() {
+		this.logger.log('launching cropper');
+		var image = this.el.nativeElement.querySelector('#imageCropper img');
+		if(this.cropper) {
+			this.cropper.destroy();
+		}
+		this.cropper = new Cropper(image, {
+		  aspectRatio: 3 / 2,
+		  preview: '#croppedWebcamPreview',
+		  zoomable: false,
+		  crop: function(e) {
+			//console.log(e.detail.x);
+			//console.log(e.detail.y);
+			//console.log(e.detail.width);
+			//console.log(e.detail.height);
+			//console.log(e.detail.rotate);
+			//console.log(e.detail.scaleX);
+			//console.log(e.detail.scaleY);
+		  }
+		});
+	}
+	
+	public onResize(event) {
+		this.logger.log('window resize');
+        this.initCropper();
+    }
+	
+	
 	
 	public  requestCalibration() {
 		this.cameraService.requestImages();
