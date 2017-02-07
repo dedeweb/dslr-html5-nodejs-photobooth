3cd6e6e9fba95e9802c76cc3b3a797fd0ee54472
Denis <denis.messie@gmail.com>
2017-02-07 09:58:51 +0100

    some cleaning

--- old/src_back/fakestream.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
- cat src_back/test.mjpeg | ffmpeg -loglevel quiet -i pipe:0 -c:v libtheora -q:v 7 -f ogg  pipe:1
--- old/src_back/stream.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
- gphoto2 --capture-movie --stdout | ffmpeg -loglevel quiet -i pipe:0 -c:v libtheora -q:v 7 -f ogg  pipe:1
